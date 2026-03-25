b1a5ef13a8a8e7f9c9f6ba178a1bd25aa3deb3be
Adityak087 <adityakute0087@gmail.com>
2026-03-25 13:47:25 +0530

    origins

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://context-graph-iyd5.vercel.app"
+        "https://context-graph-iyd5.vercel.app",
+        "https://context-graph-two.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
